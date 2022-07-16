c6c43c48fc03abd7ffad8639b7b47978810d4873
s10pao <99454468+s10pao@users.noreply.github.com>
2022-07-16 22:23:06 +0800

    Update action.c

--- quantum/action.c
+++ quantum/action.c
@@ -370,6 +370,7 @@ static void bilateral_combinations_tap(keyevent_t event) {
         }
         else if(bilateral_combinations.mods & MOD_MASK_AG) {
             send_keyboard_report();
+            wait_ms(50);
         }
         bilateral_combinations.active = false;
     }
