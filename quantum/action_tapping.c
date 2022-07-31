d37b96bdb5e219d23691b9dca7d67fd5eeae0184
s10pao <s10pao@yahoo.com>
2022-07-31 08:08:49 +0800

    test

--- quantum/action_tapping.c
+++ quantum/action_tapping.c
@@ -269,19 +269,19 @@ bool process_tapping(keyrecord_t *keyp) {
 #    endif
                     }
 
-                    if (!IS_NOEVENT(event) && tapping_key.event.key.row % (MATRIX_ROWS / 2) != event.key.row % (MATRIX_ROWS / 2)) {
-                        action_t action = layer_switch_get_action(tapping_key.event.key);
-                        switch (action.kind.id) {
-                            case ACT_LMODS_TAP:
-                            case ACT_RMODS_TAP:
-                                if (action.key.mods == 0x02) {
-                                    tapping_key.event.time -= 25;
-                                    event.time -= 75;
-                                    // keyp->tap.count = 1;
-                                }
-                                break;
-                        }
-                    }
+                    // if (!IS_NOEVENT(event) && tapping_key.event.key.row % (MATRIX_ROWS / 2) != event.key.row % (MATRIX_ROWS / 2)) {
+                    //     action_t action = layer_switch_get_action(tapping_key.event.key);
+                    //     switch (action.kind.id) {
+                    //         case ACT_LMODS_TAP:
+                    //         case ACT_RMODS_TAP:
+                    //             if (action.key.mods == 0x02) {
+                    //                 tapping_key.event.time -= 25;
+                    //                 event.time -= 75;
+                    //                 // keyp->tap.count = 1;
+                    //             }
+                    //             break;
+                    //     }
+                    // }
 
                     // enqueue
                     return false;
